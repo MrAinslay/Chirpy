04e81375ae817ee6fd254b3955d812a184666bc6
MrAinslay <asteriospaligiannis@gmail.com>
2023-12-30 16:54:46 +0200

    logo

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	const port = "8080"
+	const filepath = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
 	corsMux := middlewareCors(mux)
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepath))))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -32,4 +35,10 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8 ")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
